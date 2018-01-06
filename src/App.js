a76fc214eb1abd62ba7f2e3d872d70002339bcb7
tayathorn <EksPan@EksPan.local>
2018-01-06 22:51:38 +0700

    greeting message

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+
+export default class App extends Component {
+  state = {
+    greetingMessage: 'Welcome !',
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.greetingMessage}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
