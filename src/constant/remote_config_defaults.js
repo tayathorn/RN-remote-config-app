b1e2fe99b3e4056ee6a131955f24e8efea23f501
tayathorn <EksPan@EksPan.local>
2018-01-07 00:29:05 +0700

    change greeting message

--- src/constant/remote_config_defaults.js
+++ src/constant/remote_config_defaults.js
@@ -1,5 +1,5 @@
 const remoteConfigDefaults = {
-    greeting_message: 'Hello ja ~',
+    greeting_message: `Hello It's me ~`,
     event_picture: 'https://s3-ap-southeast-1.amazonaws.com/reacho/contentimage/httpimages/231016/img_20160510332334580ca716c822f.png',
     is_show_event_picture: true,
     button_color: 'teal',
